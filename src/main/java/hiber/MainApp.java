f621b4dee8caa529de44da3e5acae4cd0c43eadb
AnnaLeo97 <30714027+AnnaLeo97@users.noreply.github.com>
2020-09-09 13:32:39 +0500

    New Car() class added

--- src/main/java/hiber/MainApp.java
+++ src/main/java/hiber/MainApp.java
@@ -25,25 +25,25 @@ public class MainApp {
       user1.addCarToUser(ferrari);
       System.out.println(user1.getCar().getName());
       userService.add(user1);
-//
-//      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
-//      Car ford = new Car("Ford", 2);
-//      ford.setUser(user2);
-//      user2.addCarToUser(ford);
-//      userService.add(user2);
-//
-//
-//      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
-//      Car lada = new Car("Lada", 5);
-//      lada.setUser(user3);
-//      user3.addCarToUser(lada);
-//      userService.add(user3);
-//
-//      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
-//      Car yaz = new Car("Yaz", 4);
-//      yaz.setUser(user3);
-//      user4.addCarToUser(yaz);
-//      userService.add(user4);
+
+      User user2 = new User("User2", "Lastname2", "user2@mail.ru");
+      Car ford = new Car("Ford", 2);
+      ford.setUser(user2);
+      user2.addCarToUser(ford);
+      userService.add(user2);
+
+
+      User user3 = new User("User3", "Lastname3", "user3@mail.ru");
+      Car lada = new Car("Lada", 5);
+      lada.setUser(user3);
+      user3.addCarToUser(lada);
+      userService.add(user3);
+
+      User user4 = new User("User4", "Lastname4", "user4@mail.ru");
+      Car yaz = new Car("Yaz", 4);
+      yaz.setUser(user3);
+      user4.addCarToUser(yaz);
+      userService.add(user4);
 
       List<User> users = userService.listUsers();
       for (User user : users) {
